47ed78fd896d09b7e17e3f1e25f8a5fef4c53ff4
daniahnaf22 <ahnaffalihdani@gmail.com>
2022-06-22 19:00:08 +0700

    Updates

---
+++ precache-manifest.e2f4cbf008305787cb9e93e2c5d92898.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cb87d51119f06bf1456",
+    "url": "/todolist/static/css/main.61bae91c.chunk.css"
+  },
+  {
+    "revision": "7cb87d51119f06bf1456",
+    "url": "/todolist/static/js/main.7cb87d51.chunk.js"
+  },
+  {
+    "revision": "673dda70074263ed0395",
+    "url": "/todolist/static/js/1.673dda70.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "f4efc95c94bae30d40b645fff37e0292",
+    "url": "/todolist/index.html"
+  }
+];
